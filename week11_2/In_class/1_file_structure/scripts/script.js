8b1a77daeb09c91b5d29d836921bb6eacbc2b124
rutaitp <rk2906@nyu.edu>
2017-11-09 12:00:14 -0500

    updated examples

--- week11_2/In_class/1_file_structure/scripts/script.js
+++ week11_2/In_class/1_file_structure/scripts/script.js
@@ -1,8 +1,1 @@
 console.log("hello");
-
-function changeColor() {
-	//get an element with id hello
-	var newColor = document.getElementById("hello");
-	// access style and change backgroundColor property 
-	newColor.style.backgroundColor = "lightgreen";
-}
