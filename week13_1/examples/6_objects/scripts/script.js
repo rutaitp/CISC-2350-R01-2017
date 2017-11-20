6cb092d7e1082d8af38f4e572cfceefbfea33017
rutaitp <rk2906@nyu.edu>
2017-11-20 15:07:23 -0500

    slides

---
+++ week13_1/examples/6_objects/scripts/script.js
@@ -0,0 +1,13 @@
+var hotel = {
+	name: "Radisson",
+	rooms: 55,
+	booked: 15,
+	spa: false,
+	roomTypes: ["single", "twin", "suite"]
+}
+
+console.log(hotel);
+
+hotel.liked = true;
+
+console.log(hotel);
